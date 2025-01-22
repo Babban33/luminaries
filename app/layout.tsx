da66951970218889acc105137beff53199588160
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-22 15:21:26 +0530

    added flex layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-1">
+          {children}
+        </main>
+      </div>
       </body>
     </html>
   );

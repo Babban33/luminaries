66defcdc43a8c24e0e3bbbf701a76b2c90f3d3be
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-22 16:23:35 +0530

    added theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,15 +28,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      <div className="flex flex-col min-h-screen">
-        <Navbar/>
-        <main className="flex-grow mt-16">
-          <div className="min-w-screen mx-auto px-4">
-            {children}
-          </div>
-        </main>
-        <Footer/>
-      </div>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <div className="flex flex-col min-h-screen">
+          <Navbar/>
+          <main className="flex-grow">
+            <div className="min-w-screen mx-auto px-4">
+              {children}
+            </div>
+          </main>
+          <Footer/>
+        </div>
+      </ThemeProvider>
       </body>
     </html>
   );

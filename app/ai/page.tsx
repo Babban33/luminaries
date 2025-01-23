c4345b4087b3c152cf7f7c5012d9758da3e715af
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-23 11:18:06 +0530

    added link and padding

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,11 +1,14 @@
 import { Note } from "@/components/Note";
+import Link from "next/link";
 
 export default function AIHomePage(){
     return(
-        <div className="space-y-6 max-w-7xl mx-auto justify-center mt-12">
+        <div className="space-y-6 max-w-7xl mx-auto justify-center my-12 px-4 md:px-0">
             <h1 className="text-3xl font-bold">AI Notes</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <Note title="What is AI?" content="Definition, Types, Examples. Strong AI, Weak AI"/>
+                <Link href="/ai/what-is-ai">
+                    <Note title="What is AI?" content="Definition, Types, Examples. Strong AI, Weak AI"/>
+                </Link>
             </div>
         </div>
     )
